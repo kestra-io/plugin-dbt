49d214cd4a2711aa6973bda5089f869584dbd4fc
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:28:36 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/dbt/cli/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using dbt with its CLI.\n" +
+        "dbt is a data transformation tool that enables data analysts and engineers to transform, test and document data in the cloud data warehouse.",
+    categories = PluginSubGroup.PluginCategory.TOOL
+)
+package io.kestra.plugin.dbt.cli;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
---
+++ src/main/java/io/kestra/plugin/dbt/cloud/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using DBT Cloud.\n" +
+        "dbt is a data transformation tool that enables data analysts and engineers to transform, test and document data in the cloud data warehouse.",
+    categories = PluginSubGroup.PluginCategory.TOOL
+)
+package io.kestra.plugin.dbt.cloud;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
