37e9262972654315fdd3243534aecb3008620364
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-05-31 16:20:34 +0200

    feat(dbt): add deps command

---
+++ src/main/java/io/kestra/plugin/dbt/cli/Deps.java
@@ -0,0 +1,23 @@
+package io.kestra.plugin.dbt.cli;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Invoke dbt `deps` command"
+)
+public class Deps extends AbstractRun {
+    @Override
+    protected String command() {
+        return "deps";
+    }
+}
