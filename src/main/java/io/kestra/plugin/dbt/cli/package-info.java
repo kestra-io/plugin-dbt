3a828ecca0894079e6784d9644365191ac172201
YannC <ycoornaert@kestra.io>
2024-06-10 10:01:23 +0200

    chore(): updated titles

--- src/main/java/io/kestra/plugin/dbt/cli/package-info.java
+++ src/main/java/io/kestra/plugin/dbt/cli/package-info.java
@@ -1,4 +1,5 @@
 @PluginSubGroup(
+    title = "Dbt CLI",
     description = "This sub-group of plugins contains tasks for using dbt with its CLI.\n" +
         "dbt is a data transformation tool that enables data analysts and engineers to transform, test and document data in the cloud data warehouse.",
     categories = PluginSubGroup.PluginCategory.TOOL
--- src/main/java/io/kestra/plugin/dbt/cloud/package-info.java
+++ src/main/java/io/kestra/plugin/dbt/cloud/package-info.java
@@ -1,4 +1,5 @@
 @PluginSubGroup(
+    title = "Dbt Cloud",
     description = "This sub-group of plugins contains tasks for using DBT Cloud.\n" +
         "dbt is a data transformation tool that enables data analysts and engineers to transform, test and document data in the cloud data warehouse.",
     categories = PluginSubGroup.PluginCategory.TOOL
