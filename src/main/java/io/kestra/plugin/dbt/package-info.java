d5782f62c82a80e87e33fe5ff91e5ce956257ba1
Malaydewangan09 <malaydewangan310@gmail.com>
2025-08-12 23:44:22 +0530

    fix(): plugin name for better readability

--- src/main/java/io/kestra/plugin/dbt/package-info.java
+++
@@ -1,9 +0,0 @@
-@PluginSubGroup(
-    title = "DBT",
-    description = "This sub-group of plugins contains tasks for interacting with DBT.\n" +
-        "DBT is a data transformation tool that enables data analysts and engineers to transform, test and document data in the cloud data warehouse.",
-    categories = PluginSubGroup.PluginCategory.TOOL
-)
-package io.kestra.plugin.dbt;
-
-import io.kestra.core.models.annotations.PluginSubGroup;
