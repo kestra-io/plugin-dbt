5b265063db173288f3613eced5c51434ee275ffe
ben8t <pimpaudben@gmail.com>
2026-01-20 09:55:01 +0100

    chore: fix plugin categories

--- src/main/java/io/kestra/plugin/dbt/cli/package-info.java
+++ src/main/java/io/kestra/plugin/dbt/cli/package-info.java
@@ -2,7 +2,6 @@
     title = "dbt CLI",
     description = "This sub-group of plugins contains tasks for using dbt with its CLI.\n" +
         "dbt is a data transformation tool that enables data analysts and engineers to transform, test and document data in the cloud data warehouse.",
-    categories = PluginSubGroup.PluginCategory.TOOL,
     categories = {
         PluginSubGroup.PluginCategory.DATA
     }
